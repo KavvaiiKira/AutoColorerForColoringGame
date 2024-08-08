22958e3b2565500467de1e56bc6ec5a370c538e0
Aleksei Ezerski <aezerski@click2.money>
2024-08-08 20:36:29 +0300

    Fix screenshot location & Added main infinite loop & Fix key pressed time

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import win32con
 import time
 
 top = 105
-left = 180
-width = 1000
+left = 105
+width = 1125
 height = 1095
 
 
@@ -39,28 +39,29 @@ def click(x, y):
 
 def set_next_color():
     win32api.keybd_event(0x45, 0, 0, 0)
-    time.sleep(0.5)
+    time.sleep(0.25)
     win32api.keybd_event(0x45, 0, win32con.KEYEVENTF_KEYUP, 0)
 
 
 def main():
-    print("Set number of colors:")
-    number_of_colors = input()
+    while True:
+        print("Set number of colors:")
+        number_of_colors = input()
 
-    for color_number in range(1, int(number_of_colors) + 1):
-        screenshot_file_name = get_screenshot()
-        pixels_to_color_cords = get_pixels_to_color_cords(screenshot_file_name)
-
-        while pixels_to_color_cords is not None and len(pixels_to_color_cords) != 0:
-            click(pixels_to_color_cords[0][0][0] + left, pixels_to_color_cords[0][0][1] + top)
-
-            os.remove(screenshot_file_name)
+        for color_number in range(1, int(number_of_colors) + 1):
             screenshot_file_name = get_screenshot()
             pixels_to_color_cords = get_pixels_to_color_cords(screenshot_file_name)
 
-        os.remove(screenshot_file_name)
+            while pixels_to_color_cords is not None and len(pixels_to_color_cords) != 0:
+                click(pixels_to_color_cords[0][0][0] + left, pixels_to_color_cords[0][0][1] + top)
 
-        set_next_color()
+                os.remove(screenshot_file_name)
+                screenshot_file_name = get_screenshot()
+                pixels_to_color_cords = get_pixels_to_color_cords(screenshot_file_name)
+
+            os.remove(screenshot_file_name)
+
+            set_next_color()
 
 
 if __name__ == "__main__":
